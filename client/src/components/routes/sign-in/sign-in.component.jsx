93052e64d5658c5c585d6b7c287b8b0309d7013f
Mateo <mtaycruz@gmail.com>
2022-06-09 12:42:14 -0400

    created teh sign up compnent the form input component as wel as the button compnent

--- client/src/components/routes/sign-in/sign-in.component.jsx
+++ client/src/components/routes/sign-in/sign-in.component.jsx
@@ -1,94 +1,53 @@
-import React, { useState } from 'react'
-// import { SignUpForm } from '../../sign-up-form/sign-up-form.component'
+import React from 'react'
+import { SignUpForm } from '../../sign-up-form/sign-up-form.component'
 
 export const SignIn = () => {
 
-const [emailReg, setEmailReg ] = useState('')
-const [passwordReg, setPasswordReg ] = useState('')
-const [confirmPasswordReg, setConfirmPasswordReg ] = useState('')
-const [nameReg, setNameReg] = useState('')
-
-const [loginEmail, setLoginEmail] = useState('')
-const [loginPassword, setLoginPassword] = useState('')
 
 
-const registerData = { emailReg, passwordReg, nameReg }
-const loginData = { loginEmail, loginPassword}
+// const [loginEmail, setLoginEmail] = useState('')
+// const [loginPassword, setLoginPassword] = useState('')
 
-const signup = async(e)  => {
-  e.preventDefault();
+
+
+// const loginData = { loginEmail, loginPassword}
+
+
+
+// const signin = async(e)  => {
+//   e.preventDefault();
+
+//   const response = await fetch('http://localhost:3001/sign-in', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(loginData)
+//   })
+
+//   const json = await response.json()
   
-  if(passwordReg !== confirmPasswordReg){
-    alert("passwords do not match")
-    return;
-  }
-
-  const response = await fetch('http://localhost:3001/sign-up', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(registerData)
-  })
-  const json = await response.json()
-
-  if(json.message){
-    alert(json.message)
-  } else {
-    alert('Succesfully signed up!')
-  }
-
-}
-
-const signin = async(e)  => {
-  e.preventDefault();
-
-  const response = await fetch('http://localhost:3001/sign-in', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(loginData)
-  })
-
-  const json = await response.json()
-  
-  if(json.message){
-    alert(json.message)
-  } else {
-    alert('logged in')
-  }
+//   if(json.message){
+//     alert(json.message)
+//   } else {
+//     alert('logged in')
+//   }
   
 
-}
+// }
 
   return (
     <div>
-      <h1>Sign In</h1>
+      {/* <h1>Sign In</h1>
       <form>
         <label>Email</label>
         <input type="email" placeholder='Email...' onChange={(e) => {setLoginEmail(e.target.value)}} />
         <input type="password" placeholder='Password...' onChange={(e) => {setLoginPassword(e.target.value)}}/>
         <button onClick={signin}>Sign In</button>
-      </form>
+      </form> */}
 
+      <SignUpForm />
 
-      <h1>Sign up with your email and password</h1>
-        <form>
-            <label htmlFor="">Display Name</label>
-            <input type="text" required onChange={(e) => {setNameReg(e.target.value)}}/>
-
-            <label htmlFor="" >email</label>
-            <input type="email" required onChange={(e)=>{setEmailReg(e.target.value)}} />
-
-            <label htmlFor="" >Password</label>
-            <input type="password"required onChange={(e)=>{setPasswordReg(e.target.value)}} />
-
-            <label htmlFor="">Confirm Password</label>
-            <input type="password"required onChange={(e)=>{setConfirmPasswordReg(e.target.value)}}/>
-
-            <button type="submit" onClick={signup}>Sign up</button>
-        </form>
     </div>
   )
 }
--- client/src/components/sign-up-form/sign-up-form.component.jsx
+++ client/src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,23 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import { FormInput } from '../form-input/FormInput.component'
+
+import { Button } from '../button/button.component'
+
+import './sign-up-form.style.scss'
+
+const defaultFormFields = {
+  displayName: '',
+  email: '',
+  password: '',
+  confirmPassword: ''
+}
 
 export const SignUpForm = () => {
+
+  const [formFields, setFormFields ] = useState(defaultFormFields)
+  const { displayName, email, password, confirmPassword} = formFields
+
+  const registerData = { displayName, email, password }
+
+  const handleChange = (event) => {
+    const { name, value } = event.target
+  
+    setFormFields({ ...formFields, [name]: value})
+  }
+  
+  const signup = async(e)  => {
+    e.preventDefault();
+    
+    if(password !== confirmPassword){
+      alert("passwords do not match")
+      return;
+    }
+    
+    try{
+      const response = await fetch('http://localhost:3001/sign-up', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(registerData)
+      })
+      const json = await response.json()
+      alert(json.message)
+    
+    } catch(error){
+      console.log(error)
+    }
+  
+  }
+
   return (
-    <div>
-        <h1>Sign up with your email and password</h1>
-        <form onSubmit={() => {}}>
-            <label htmlFor="">Display Name</label>
-            <input type="text" required />
+    <div className='sign-up-container'>
 
-            <label htmlFor="">email</label>
-            <input type="email" required />
+      <h2>Don't have an account?</h2>
+      <span>Sign up with your email and password</span>
+        <form onSubmit={signup}>
+            
+            <FormInput label="Display Name" type="text" required onChange={handleChange} name="displayName" value={displayName}/>
 
-            <label htmlFor="">Password</label>
-            <input type="password"required />
+            <FormInput label="Email" type="email" required onChange={handleChange} name="email" value={email}/>
 
-            <label htmlFor="">Confirm Password</label>
-            <input type="password"required />
+            <FormInput label="Password" type="password"required onChange={handleChange} name="password" value={password}/>
 
-            <button type="submit">Sign up</button>
+            <FormInput label="Confirm Password" type="password"required onChange={handleChange} name="confirmPassword" value={confirmPassword}/>
+
+            <Button type="submit" >Sign up</Button>
         </form>
     </div>
   )
